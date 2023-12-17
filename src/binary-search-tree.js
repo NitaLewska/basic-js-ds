eee39b5a86e7f1b62ba11fda8cf2d9792ec1d0a9
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-12-18 01:03:01 +0300

    feat: implemented root(), add(), has(), find() in binary-search-tree; 10/20 (01:02:51 Monday, December 18, 2023)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,71 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  head = null
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head ? this.head : null
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (!this.head) {
+      this.head = new Node(data)
+      console.log(this.head)
+      return
+    }
+    let node = this.head
+    while (node) {
+      if (data > node.data) {
+        if (!node.right) {
+          node.right = new Node(data)
+          return
+        }
+        node = node.right
+      } else if (data < node.data){
+         if (!node.left) {
+          node.left = new Node(data)
+          return
+        }
+        node = node.left
+      }
+    }
+
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let node = this.head
+    while (node) {
+      if (data === node.data) {
+        return true
+      }
+      if (data > node.data) {
+        node = node.right
+      } else if (data < node.data) {
+        node = node.left
+      }
+    }
+    return false
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let node = this.head
+    while (node) {
+      if (data == node.data) {
+        return node
+      }
+      if (data > node.data) {
+        node = node.right
+      } else if (data < node.data) {
+        node = node.left
+      }
+    }
+    return null
   }
 
   remove(/* data */) {
